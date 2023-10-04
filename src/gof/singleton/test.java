7d6c8ef703147aefbc8df7d551b2ed95a510bbb6
RodrigoChinen <eng.chinen@gmail.com>
2023-10-04 14:56:48 -0300

    Correções

---
+++ src/gof/singleton/test.java
@@ -0,0 +1,24 @@
+package gof.singleton;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        // Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+    }
+}
